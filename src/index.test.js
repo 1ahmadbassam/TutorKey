a370058c65873963441b8c322de2d0baf80ebd68
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2023-09-19 00:37:03 +0300

    Jest-related config. As soon as codebase becomes more complex, having tests will be feasible.

---
+++ src/index.test.js
@@ -0,0 +1,16 @@
+/** Non-functional code for unit tests on default HTML items
+
+import './styles/normalize.css';
+import './styles/index.css';
+
+const getDocumentElement = require("./index")
+
+test("Check if Root element is not broken", () => {
+    expect(getDocumentElement("#root").toString()).toBe("<div id=\"root\">");
+});
+
+test("Check if font-size on webpage HTML is not altered", () => {
+    expect(getDocumentElement("html").style.fontSize).toBe("16px");
+});
+
+ */
