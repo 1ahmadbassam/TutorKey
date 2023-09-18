f781d9c8848436b5676c51a795f259f19fe93717
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2023-09-18 23:34:28 +0300

    Initial commit
    Fix project structure to suit our needs
    Clear out unnecessary placeholders
    Update .gitignore to ignore Webstorm cache files

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    // TODO: Fill footer
+    return (
+        <footer className="footer">
+            <p>Footer!</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    // TODO: Fill header
+    return (
+        <nav className="header">
+            <h1>Header!</h1>
+        </nav>
+    )
+}
+
+export default Header;
