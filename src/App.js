1254a7093b5b1865b0cdcbc3e17e4e98f2690f20
1ahmadbassam <1ahmadbassam@users.noreply.github.com>
2023-09-19 22:06:02 +0000

    Style: Format Files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./Landing";
 
 function App() {
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Landing />}/>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
