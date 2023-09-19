89e80d33918b62539b8f5caa0854cc19a7e7fc81
1ahmadbassam <1ahmadbassam@users.noreply.github.com>
2023-09-19 20:24:25 +0000

    Style: Format Files

--- src/Landing.js
+++ src/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./styles/shared.css";
-import "./styles/index.css"
+import "./styles/index.css";
 
 function Landing() {
   return (
