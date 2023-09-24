2a2fb3f12b3771cd1551ba080635621825b6a235
KarimHabbal1 <kmhabbal@gmail.com>
2023-09-24 23:10:32 +0300

    Adding first page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import FirstPage from "./components/FirstPage";
 
 function Landing() {
   return (
     <div className="page landing">
       <Header />
+      <FirstPage />
       <Footer />
       <Outlet />
     </div>
