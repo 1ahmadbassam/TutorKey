b0719895519299c4d4dc4f3a3ae563753f15296f
1ahmadbassam <1ahmadbassam@users.noreply.github.com>
2023-09-27 22:24:44 +0000

    Style: Format Files

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -11,13 +11,13 @@ import SearchBox from "./components/searchpage/SearchBox";
 
 function SearchPage() {
   return (
-      <div className="page search-page">
-        <Header />
-        <SearchBox />
-        <SearchCards />
-        <Footer />
-        <Outlet />
-      </div>
+    <div className="page search-page">
+      <Header />
+      <SearchBox />
+      <SearchCards />
+      <Footer />
+      <Outlet />
+    </div>
   );
 }
 
